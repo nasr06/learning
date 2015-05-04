ed52f362a00017e277bda5c1cd523d89b4da6bae
nasr06 <nasreddine.mhanni@gmail.com>
2015-05-04 17:36:49 +0100

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/{pays}/{ville}', function($pays,$ville) {
+
+	$nom_table="GeoPC_".$pays;
+	
+			$villes= DB::table($nom_table)->select(DB::raw('City, ZIP'));
+	
+
+	if (preg_match('#[0-9]#',$ville)){
+		
+									  $villes= $villes->where('ZIP','LIKE',"$ville%")
+									  ->get();
+
+	}else{
+		$villes= $villes->where('City','LIKE',"$ville%")->get();
+
+	}
+
+	return Response::json($villes);
+});
+
+	
